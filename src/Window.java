b951835c75d19c26a554b8473e1f9c35be8e7b8c
oskalbarczyk <oskalbarczyk@gmail.com>
2023-05-15 17:16:35 +0200

    init

---
+++ src/Window.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+
+public class Window extends JFrame {
+    private static Window instance;
+
+    private Window() {
+    super("GuiSet03");
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setSize(1280,720);
+    setVisible(true);
+
+
+    }
+
+    /**
+     * Algorytm Singleton
+     * @return Przy każdym wywołaniu zwraca ten sam obiekt klasy Window, dzięki czemu mamy pewność, że w programie
+     * powstanie tylko jeden obiekt klasy Window.
+     */
+    public static Window getInstance() {
+        if (instance == null) {
+            instance = new Window();
+        }
+        return instance;
+    }
+}
